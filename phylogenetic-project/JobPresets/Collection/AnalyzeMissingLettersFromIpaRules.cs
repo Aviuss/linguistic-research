1f1ee36798470aeac2932be945790d888e60b4a8
Aviuss <konradbb09@gmail.com>
2026-09-23 14:54:57 +0200

    configure normalization rules

--- phylogenetic-project/JobPresets/Collection/AnalyzeMissingLettersFromIpaRules.cs
+++ phylogenetic-project/JobPresets/Collection/AnalyzeMissingLettersFromIpaRules.cs
@@ -40,8 +40,6 @@ public class AnalyzeMissingLettersFromIpaRules : IJobPreset
         this.languageRulesWrapper = languageRulesWrapper;
         this.mapIdbToName = mapIdbToName;
         this.ipaLetterDistanceDict = ipaLetterDistanceDict;
-        this.textLetterBlackList =  new() {"-", ",", ":", "!", "?", ".", "…", "“", "”", "„", "(", ")", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "–", "—", ";", "«", "»", "\"", "‑", " ", " ", "­", "̈"};
-
     }
 
     public void Start()
